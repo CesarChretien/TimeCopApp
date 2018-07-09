febb62cdbada1ecff2d136cc9bd0cfc548c1db65
cesarchretien <cesarchre10@gmail.com>
2018-07-10 01:02:00 +0200

    sort of done.

--- app/src/main/java/com/cesarchretien/timecopapp/TimeCopActivity.kt
+++ app/src/main/java/com/cesarchretien/timecopapp/TimeCopActivity.kt
@@ -4,8 +4,18 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_time_cop.*
 import java.util.concurrent.TimeUnit
+import android.media.MediaPlayer
+import android.graphics.Color
+import android.support.annotation.RawRes
+import android.util.Log
+import java.io.IOException
+import java.util.concurrent.atomic.AtomicBoolean
+
 
 class TimeCopActivity : AppCompatActivity() {
+
+    var isStopped = false
+    val hasWarned = AtomicBoolean(false)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,23 +23,72 @@ class TimeCopActivity : AppCompatActivity() {
 
         button_start.setOnClickListener {
             val amount = Integer.valueOf(time_minutes_amount.text.toString()).toLong()
+            val warning = TimeUnit.MINUTES.toMillis(Integer.valueOf(time_minutes_amount_warning.text.toString()).toLong())
             val start = System.currentTimeMillis()
             val end = start + TimeUnit.MINUTES.toMillis(amount)
-            refreshTime(start, end)
+            val thread = Thread(Runnable { refreshTime(start, end, warning) })
+            thread.start()
+
+            button_reset.setOnClickListener {
+                isStopped = true
+                hasWarned.set(false)
+                stopwatch_text.setTextColor(Color.WHITE)
+            }
         }
     }
 
-    private fun refreshTime(current: Long, end: Long) {
+    private fun refreshTime(current: Long, end: Long, warning: Long) {
         if (current < end) {
             stopwatch_text.postDelayed({
-                val currentTime = end - System.currentTimeMillis()
-                val currentSecondsRaw = currentTime / 1000
-                val currentMinutes = currentSecondsRaw / 60
-                val currentSeconds = currentSecondsRaw % 60
-                val currentMillis = current % 1000
-                stopwatch_text.text = "$currentMinutes : $currentSeconds : $currentMillis"
-                refreshTime(System.currentTimeMillis(), end)
-            }, 100L)
+                if (!isStopped) {
+                    setRemainingTime(end, current, warning)
+                    refreshTime(System.currentTimeMillis(), end, warning)
+                } else {
+                    isStopped = false
+                    val amountR = Integer.valueOf(time_minutes_amount.text.toString()).toLong()
+                    val startR = System.currentTimeMillis()
+                    val endR = startR + TimeUnit.MINUTES.toMillis(amountR)
+                    setRemainingTime(endR, startR, warning)
+                }
+            }, 50L)
+        } else {
+            stopwatch_text.postDelayed({
+                setRemainingTime(end, end, warning)
+                stopwatch_text.setTextColor(Color.WHITE)
+                play(R.raw.smb_stage_clear)
+            }, 50L)
+        }
+    }
+
+    private fun setRemainingTime(end: Long, current: Long, warning: Long) {
+        val remaining = end - current
+
+        if (remaining < warning && !hasWarned.getAndSet(true)) {
+            stopwatch_text.setTextColor(Color.RED)
+            play(R.raw.smb_warning)
+        }
+
+        val remainingSecondsRaw = remaining / 1000
+        val remainingMinutes = String.format("%02d", remainingSecondsRaw / 60)
+        val remainingSeconds = String.format("%02d", remainingSecondsRaw % 60)
+        val remainingMillis = String.format("%03d", remaining % 1000)
+        stopwatch_text.text = "$remainingMinutes : $remainingSeconds : $remainingMillis"
+    }
+
+    private fun play(@RawRes musicRaw: Int) {
+        val afd = resources.openRawResourceFd(musicRaw)
+        val fileDescriptor = afd.fileDescriptor
+        val player = MediaPlayer()
+        try {
+            player.setDataSource(fileDescriptor, afd.startOffset, afd.length)
+            player.isLooping = false
+            player.prepare()
+            player.start()
+            player.setOnCompletionListener {
+                it.release()
+            }
+        } catch (ex: IOException) {
+            Log.e("Oops", ex.localizedMessage)
         }
     }
 
