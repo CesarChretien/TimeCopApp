405176a40542eb32258a50cd740548b7ea06a2cd
Cesar Chretien <Cesar.Chretien@klm.com>
2018-07-09 18:21:48 +0200

    make timer.

--- app/src/main/java/com/cesarchretien/timecopapp/StopwatchView.kt
+++ app/src/main/java/com/cesarchretien/timecopapp/StopwatchView.kt
@@ -1,28 +1,10 @@
 package com.cesarchretien.timecopapp
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
 import android.util.AttributeSet
 import android.widget.FrameLayout
 
 class StopwatchView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : FrameLayout(context, attrs, defStyleAttr) {
 
-    override fun dispatchDraw(canvas: Canvas?) {
-        super.dispatchDraw(canvas)
 
-        val paint = Paint().apply {
-            color = Color.RED
-        }
-
-        val radius = Math.min(width, height).toFloat() / 2f
-
-        canvas?.drawCircleLine(radius, radius, radius, 20f, paint)
-    }
-
-    private fun Canvas.drawCircleLine(cx: Float, cy: Float, radius: Float, thickness: Float, paint: Paint) {
-        drawCircle(cx, cy, radius, paint)
-        drawCircle(cx, cy, radius - thickness, Paint().apply { color = Color.BLACK })
-    }
 }
--- app/src/main/java/com/cesarchretien/timecopapp/TimeCopActivity.kt
+++ app/src/main/java/com/cesarchretien/timecopapp/TimeCopActivity.kt
@@ -1,12 +1,36 @@
 package com.cesarchretien.timecopapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_time_cop.*
+import java.util.concurrent.TimeUnit
 
 class TimeCopActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_time_cop)
+
+        button_start.setOnClickListener {
+            val amount = Integer.valueOf(time_minutes_amount.text.toString()).toLong()
+            val start = System.currentTimeMillis()
+            val end = start + TimeUnit.MINUTES.toMillis(amount)
+            refreshTime(start, end)
+        }
     }
+
+    private fun refreshTime(current: Long, end: Long) {
+        if (current < end) {
+            stopwatch_text.postDelayed({
+                val currentTime = end - System.currentTimeMillis()
+                val currentSecondsRaw = currentTime / 1000
+                val currentMinutes = currentSecondsRaw / 60
+                val currentSeconds = currentSecondsRaw % 60
+                val currentMillis = current % 1000
+                stopwatch_text.text = "$currentMinutes : $currentSeconds : $currentMillis"
+                refreshTime(System.currentTimeMillis(), end)
+            }, 100L)
+        }
+    }
+
 }
